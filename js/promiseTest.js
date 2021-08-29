6806b4a68cc5eb4a6758290d1d0904cd1c68180e
Vlad <start2top@gmail.com>
2021-08-29 16:46:59 +0300

    remaked repository first commit

---
+++ js/promiseTest.js
@@ -0,0 +1,40 @@
+let form = document.querySelector('.main-form'),
+input = form.getElementsByTagName('input'),
+statusMessage = document.createElement('div');
+
+statusMessage.classList.add('status');
+
+form.addEventListener('submit', function(event){
+event.preventDefault();
+form.appendChild(statusMessage);
+
+let request = new XMLHttpRequest();
+request.open('POST', 'server.php');
+request.setRequestHeader ( 'Content-Type', 'application/x-www-form-ulrencoded');
+
+let formData = new FormData(form);
+
+
+let obj = {};
+formData.forEach(function(value, key) {
+    obj[key] = value;
+});
+
+let json = JSON.stringify(obj);
+.
+request.send(json);
+
+request.addEventListener('readystatechange', function(){
+    if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+    } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;
+    } else {
+        statusMessage.innerHTML = message.failure;
+    }
+});
+
+for (let i = 0; i < input.length; i++) {
+    input[i].value = '';
+}
+});
